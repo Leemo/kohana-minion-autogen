69692da33507504fd003ef5a9e62a5a3cb7f34b1
Alexeyco <6425762@gmail.com>
2013-04-20 23:07:39 +0400

    Previous commit fixed

--- views/minion/autogen/i18n.php
+++ views/minion/autogen/i18n.php
@@ -6,24 +6,24 @@ return array
 <?php foreach ($i18n as $term => $data): ?>
 <?php if ($data['translated']): ?>
 <?php if (is_array($data['translate'])): ?>
-	'<?php echo str_replace('\"', '', addslashes($term)) ?>' => array
+	'<?php echo addcslashes($term, '\'') ?>' => array
 	(
-		'one'  => '<?php echo str_replace('\"', '', addslashes($data['translate']['one'])) ?>',
-		'few'  => '<?php echo str_replace('\"', '', addslashes($data['translate']['few'])) ?>',
-		'many' => '<?php echo str_replace('\"', '', addslashes($data['translate']['many'])) ?>',
+		'one'  => '<?php echo addcslashes($data['translate']['one'], '\'') ?>',
+		'few'  => '<?php echo addcslashes($data['translate']['few'], '\'') ?>',
+		'many' => '<?php echo addcslashes($data['translate']['many'], '\'') ?>',
 	),
 <?php else: ?>
-	'<?php echo str_replace('\"', '', addslashes($term)) ?>' => '<?php echo str_replace('\"', '', addslashes($data['translate'])) ?>',
+	'<?php echo addcslashes($term, '\'') ?>' => '<?php echo addcslashes($data['translate'], '\'') ?>',
 <?php endif ?>
 <?php else: ?>
 <?php if($data['translate'] === Task_Autogen_I18n::SINGULAR): ?>
-	//'<?php echo str_replace('\"', '', addslashes($term)) ?>' => '<?php echo str_replace('\"', '', addslashes($term)) ?>',
+	//'<?php echo addcslashes($term, '\'') ?>' => '<?php echo addcslashes($term, '\'') ?>',
 <?php else: ?>
-	//'<?php echo str_replace('\"', '', addslashes($term)) ?>' => array
+	//'<?php echo addcslashes($term, '\'') ?>' => array
 	//(
-	//	'one'  => '<?php echo str_replace('\"', '', addslashes($term)) ?>',
-	//	'few'  => '<?php echo str_replace('\"', '', addslashes($term)) ?>',
-	//	'many' => '<?php echo str_replace('\"', '', addslashes($term)) ?>',
+	//	'one'  => '<?php echo addcslashes($term, '\'') ?>',
+	//	'few'  => '<?php echo addcslashes($term, '\'') ?>',
+	//	'many' => '<?php echo addcslashes($term, '\'') ?>',
 	//),
 <?php endif ?>
 <?php endif ?>
